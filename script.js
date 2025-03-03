89e4a88cbc7ccc1754ffbcd6a68f3d4d6c94ddee
gfloriano11 <gflorianodev@gmail.com>
2025-03-03 12:10:48 -0300

    feat: script.js pull api data [from server.js]

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#button');
 
 button.addEventListener('click', () => {
-    fetch('http://localhost:8000', {mode: "no-cors"})
+    fetch('http://localhost:8000')
     .then(response => response.json())
     .then(data => {
         console.log(data);
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const { error } = require('console');
+const cors = require('cors');
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const port = 8000;
+
+app.use(cors());
 
 const connection = mysql.createConnection({
     host: 'localhost',
